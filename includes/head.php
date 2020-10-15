191a523949c4c610fb70416574542f38cb97f299
alandry24 <alandry24@hotmail.com>
2020-10-15 13:06:41 -0500

    added more amp scripts to head, layed out amp carousel to replace home page carousel

--- includes/head.php
+++ includes/head.php
@@ -1,118 +1,222 @@
- <meta charset="utf-8"/>
-    <meta content="IE=edge" http-equiv="X-UA-Compatible">
+<meta charset="utf-8"/>
+<meta content="IE=edge" http-equiv="X-UA-Compatible">
 
 
- <meta content="" name="keywords"/>
-    <meta content="" name="author"/>
-    <meta content="" name="robots"/>
- <meta property="og:url"                content="<?php echo $Url;?>" />
- <meta property="og:title"              content="<?php echo $Title;?>" />
- <meta property="description"           content="<?php echo $metaDescription;?>" />
- <meta property="og:description"        content="<?php echo $ogMetaDescription;?>" />
- <meta property="og:image"              content="<?php echo $metaImage;?>" />
- <link rel="canonical"                  href="<?php echo $CanonicalLink?>" />
+<meta content="" name="keywords"/>
+<meta content="" name="author"/>
+<meta content="" name="robots"/>
+<meta property="og:url" content="<?php echo $Url; ?>"/>
+<meta property="og:title" content="<?php echo $Title; ?>"/>
+<meta property="description" content="<?php echo $metaDescription; ?>"/>
+<meta property="og:description" content="<?php echo $ogMetaDescription; ?>"/>
+<meta property="og:image" content="<?php echo $metaImage; ?>"/>
+<meta property="fb:app_id" content="your_app_id"/>
 
- <!-- AMP SCRIPT -->
- <style amp-boilerplate>body{-webkit-animation:-amp-start 8s steps(1,end) 0s 1 normal both;-moz-animation:-amp-start 8s steps(1,end) 0s 1 normal both;-ms-animation:-amp-start 8s steps(1,end) 0s 1 normal both;animation:-amp-start 8s steps(1,end) 0s 1 normal both}@-webkit-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-moz-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-ms-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-o-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}</style><noscript><style amp-boilerplate>body{-webkit-animation:none;-moz-animation:none;-ms-animation:none;animation:none}</style></noscript>
- <script async src="https://cdn.ampproject.org/v0.js"></script>
+<link rel="canonical" href="<?php echo $CanonicalLink ?>"/>
 
 
- <!-- FAVICONS ICON -->
-    <link href="images/favicon.ico" rel="icon" type="image/x-icon"/>
-    <link href="images/favicon.png" rel="shortcut icon" type="image/x-icon"/>
+<!-- AMP SCRIPT -->
+<style amp-boilerplate>body {
+        -webkit-animation: -amp-start 8s steps(1, end) 0s 1 normal both;
+        -moz-animation: -amp-start 8s steps(1, end) 0s 1 normal both;
+        -ms-animation: -amp-start 8s steps(1, end) 0s 1 normal both;
+        animation: -amp-start 8s steps(1, end) 0s 1 normal both
+    }
 
-    <!-- PAGE TITLE HERE -->
- <title><?php echo $Title;?></title>
-
-    <!-- MOBILE SPECIFIC -->
- <meta name="viewport" content="width=device-width, minimum-scale=1, initial-scale=1">
-
-    <!-- [if lt IE 9]>
-    <script src="js/html5shiv.min.js"></script>
-    <script src="js/respond.min.js"></script>
-    <![endif] -->
-
-    <!-- BOOTSTRAP STYLE SHEET
-        TODO compile css into sass
-        npm install -g sass
-        run command
-        sass source/stylesheets/index.scss build/stylesheets/index.css
-        to compile
-    -->
-
-
-    <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css">
-    <link href="css/fontawesome/css/font-awesome.min.css" rel="stylesheet" type="text/css"/>
-    <!-- FLATICON STYLE SHEET -->
-    <link href="css/flaticon.min.css" rel="stylesheet" type="text/css">
-    <!-- ANIMATE STYLE SHEET -->
-    <link href="css/animate.min.css" rel="stylesheet" type="text/css">
-    <!-- OWL CAROUSEL STYLE SHEET -->
-    <link href="css/owl.carousel.min.css" rel="stylesheet" type="text/css">
-    <!-- BOOTSTRAP SELECT BOX STYLE SHEET -->
-    <link href="css/bootstrap-select.min.css" rel="stylesheet" type="text/css">
-    <!-- MAGNIFIC POPUP STYLE SHEET -->
-    <link href="css/magnific-popup.min.css" rel="stylesheet" type="text/css">
-    <!-- LOADER STYLE SHEET -->
-    <link href="css/loader.min.css" rel="stylesheet" type="text/css">
-    <!-- MAIN STYLE SHEET -->
-    <link href="css/style.css" rel="stylesheet" type="text/css">
-    <link href="css/styles-embed-2019-06-10.css" rel="stylesheet" type="text/css">
-    <!-- THEME COLOR CHANGE STYLE SHEET -->
-    <link class="skin" href="css/skin/skin-12.css" rel="stylesheet" type="text/css">
-    <!-- CUSTOM  STYLE SHEET -->
-    <link href="css/custom.css" rel="stylesheet" type="text/css">
-
-
-    <!-- REVOLUTION SLIDER CSS -->
-    <link href="plugins/revolution/revolution/css/settings.css" rel="stylesheet" type="text/css">
-    <!-- REVOLUTION NAVIGATION STYLE -->
-    <link href="plugins/revolution/revolution/css/navigation.css" rel="stylesheet" type="text/css">
-
-
-    <!-- GOOGLE FONTS -->
-    <link href='https://fonts.googleapis.com/css?family=Roboto:400,100,300,300italic,400italic,500,500italic,700,700italic,900italic,900'
-          rel='stylesheet' type='text/css'>
-    <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,300,300italic,400italic,600,600italic,700,800italic,800,700italic'
-          rel='stylesheet' type='text/css'>
-    <link href="https://fonts.googleapis.com/css?family=Crete+Round:400,400i&amp;subset=latin-ext" rel="stylesheet">
-    <!-- estimator js -->
-
-    <script cid="2924" src="https://calculator.roofr.com/roofr-calculator.js"></script>
-
-    <!--
-    TODO fix this structured data
-    <script type="application/ld+json">
-        {
-         "@context": "http://schema.org",
-         "@type": "NewsArticle",
-         "mainEntityOfPage":{
-           "@type":"WebPage",
-           "@id":"https://example.com/my-article.html"
-         },
-         "headline": "My First AMP Article",
-         "image": {
-           "@type": "ImageObject",
-           "url": "https://example.com/article_thumbnail1.jpg",
-           "height": 800,
-           "width": 800
-         },
-         "datePublished": "2015-02-05T08:00:00+08:00",
-         "dateModified": "2015-02-05T09:20:00+08:00",
-         "author": {
-           "@type": "Person",
-           "name": "John Doe"
-         },
-         "publisher": {
-           "@type": "Organization",
-           "name": "⚡ AMP Times",
-           "logo": {
-             "@type": "ImageObject",
-             "url": "https://example.com/amptimes_logo.jpg",
-             "width": 600,
-             "height": 60
-           }
-         },
-         "description": "My first experience in an AMPlified world"
+    @-webkit-keyframes -amp-start {
+        from {
+            visibility: hidden
         }
+        to {
+            visibility: visible
+        }
+    }
+
+    @-moz-keyframes -amp-start {
+        from {
+            visibility: hidden
+        }
+        to {
+            visibility: visible
+        }
+    }
+
+    @-ms-keyframes -amp-start {
+        from {
+            visibility: hidden
+        }
+        to {
+            visibility: visible
+        }
+    }
+
+    @-o-keyframes -amp-start {
+        from {
+            visibility: hidden
+        }
+        to {
+            visibility: visible
+        }
+    }
+
+    @keyframes -amp-start {
+        from {
+            visibility: hidden
+        }
+        to {
+            visibility: visible
+        }
+    }</style>
+<noscript>
+    <style amp-boilerplate>body {
+            -webkit-animation: none;
+            -moz-animation: none;
+            -ms-animation: none;
+            animation: none
+        }</style>
+</noscript>
+<script async src="https://cdn.ampproject.org/v0.js"></script>
+<script async custom-element="amp-carousel" src="https://cdn.ampproject.org/v0/amp-carousel-0.1.js"></script>
+
+
+
+<!-- FAVICONS ICON -->
+<link href="images/favicon.ico" rel="icon" type="image/x-icon"/>
+<link href="images/favicon.png" rel="shortcut icon" type="image/x-icon"/>
+
+<!-- PAGE TITLE HERE -->
+<title><?php echo $Title; ?></title>
+
+<!-- MOBILE SPECIFIC -->
+<meta name="viewport" content="width=device-width, minimum-scale=1, initial-scale=1">
+
+<!-- [if lt IE 9]>
+<script src="js/html5shiv.min.js"></script>
+<script src="js/respond.min.js"></script>
+<![endif] -->
+
+<!-- BOOTSTRAP STYLE SHEET
+    TODO compile css into sass
+    npm install -g sass
+    run command
+    sass source/stylesheets/index.scss build/stylesheets/index.css
+    to compile
+-->
+
+
+<link href="css/bootstrap.min.css" rel="stylesheet" type="text/css">
+<link href="css/fontawesome/css/font-awesome.min.css" rel="stylesheet" type="text/css"/>
+<!-- FLATICON STYLE SHEET -->
+<link href="css/flaticon.min.css" rel="stylesheet" type="text/css">
+<!-- ANIMATE STYLE SHEET -->
+<link href="css/animate.min.css" rel="stylesheet" type="text/css">
+<!-- OWL CAROUSEL STYLE SHEET -->
+<link href="css/owl.carousel.min.css" rel="stylesheet" type="text/css">
+<!-- BOOTSTRAP SELECT BOX STYLE SHEET -->
+<link href="css/bootstrap-select.min.css" rel="stylesheet" type="text/css">
+<!-- MAGNIFIC POPUP STYLE SHEET -->Yeah
+<link href="css/magnific-popup.min.css" rel="stylesheet" type="text/css">
+<!-- LOADER STYLE SHEET -->
+<link href="css/loader.min.css" rel="stylesheet" type="text/css">
+<!-- MAIN STYLE SHEET -->
+<link href="css/style.css" rel="stylesheet" type="text/css">
+<link href="css/styles-embed-2019-06-10.css" rel="stylesheet" type="text/css">
+<!-- THEME COLOR CHANGE STYLE SHEET -->
+<link class="skin" href="css/skin/skin-12.css" rel="stylesheet" type="text/css">
+<!-- CUSTOM  STYLE SHEET -->
+<link href="css/custom.css" rel="stylesheet" type="text/css">
+
+
+<!-- REVOLUTION SLIDER CSS -->
+<link href="plugins/revolution/revolution/css/settings.css" rel="stylesheet" type="text/css">
+<!-- REVOLUTION NAVIGATION STYLE -->
+<link href="plugins/revolution/revolution/css/navigation.css" rel="stylesheet" type="text/css">
+
+
+<!-- GOOGLE FONTS -->
+<link href='https://fonts.googleapis.com/css?family=Roboto:400,100,300,300italic,400italic,500,500italic,700,700italic,900italic,900'
+      rel='stylesheet' type='text/css'>
+<link href='https://fonts.googleapis.com/css?family=Open+Sans:400,300,300italic,400italic,600,600italic,700,800italic,800,700italic'
+      rel='stylesheet' type='text/css'>
+<link href="https://fonts.googleapis.com/css?family=Crete+Round:400,400i&amp;subset=latin-ext" rel="stylesheet">
+<!-- estimator js -->
+
+<script cid="2924" src="https://calculator.roofr.com/roofr-calculator.js"></script>
+
+<!--
+TODO fix this structured data
+<script type="application/ld+json">
+    {
+     "@context": "http://schema.org",
+     "@type": "Roofing",
+     "mainEntityOfPage":{
+       "@type":"WebPage",
+       "@id":"https://example.com/my-article.html"
+     },
+     "headline": "My First AMP Article",
+     "image": {
+       "@type": "ImageObject",
+       "url": "https://example.com/article_thumbnail1.jpg",
+       "height": 800,
+       "width": 800
+     },
+     "datePublished": "2015-02-05T08:00:00+08:00",
+     "dateModified": "2015-02-05T09:20:00+08:00",
+     "author": {
+       "@type": "Person",
+       "name": "John Doe"
+     },
+     "publisher": {
+       "@type": "Organization",
+       "name": "⚡ AMP Times",
+       "logo": {
+         "@type": "ImageObject",
+         "url": "https://example.com/amptimes_logo.jpg",
+         "width": 600,
+         "height": 60
+       }
+     },
+     "description": "My first experience in an AMPlified world"
+    }
 </script>-->
+
+<!-- NEW SCHEMA-->
+<!-- <script type="application/ld+json">
+{
+	"@context": "https://schema.org",
+	"@type": "GeneralContractor",
+	"name": "ARS Construction",
+	"address": {
+		"@type": "PostalAddress",
+		"streetAddress": "16943 Manchester Rd",
+		"addressLocality": "Wildwood",
+		"addressRegion": "MO",
+		"postalCode": "63040"
+	},
+	"email": "ARSOffice@advancedroofsys.com",
+	"telePhone": "(844) 445-ROOF",
+	"url": "https://www.advancedroofsys.com/",
+	"paymentAccepted": [ "credit card", "invoice" ],
+	"openingHours": "Mo,Tu,We,Th,Fr,Sa ",
+	"openingHoursSpecification": [ {
+		"@type": "OpeningHoursSpecification",
+		"dayOfWeek": [
+			"Monday",
+			"Tuesday",
+			"Wednesday",
+			"Thursday",
+			"Friday",
+			"Saturday"
+		],
+		"opens": "Open",
+	} ],
+	"geo": {
+		"@type": "GeoCoordinates",
+		"latitude": "38.576384",
+		"longitude": "-90.636149"
+	},
+	"priceRange":"$"
+
+}
+</script>
+-->
